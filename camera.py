9974e18aabfd6d2d0697005a4f26b0e2408c2b1b
Jack Ha <jackha@gmail.com>
2013-11-15 23:06:04 +0100

    testing

--- camera.py
+++ camera.py
@@ -1,6 +1,18 @@
+print 'initializing...'
 from SimpleCV import Camera
 
 if __name__ == '__main__':
+	print 'init camera...'
 	cam = Camera({"width": 640, "height": 480})
 	img = cam.getImage()
-	img.save('joepie.jpg')
+
+	print 'detecting faces...'
+	faces = img.findHaarFeatures("haar/haarcascade_frontalface_alt.xml")
+
+	#print locations
+	for f in faces:
+		print "I found a face at " + str(f.coordinates())
+
+	#outline who was drinking last night (or at least has the greenest pallor)
+	faces.sortColorDistance(Color.GREEN)[0].draw(Color.GREEN)
+	img.save("greenest_face_detected.png")
