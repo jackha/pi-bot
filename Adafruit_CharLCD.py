074c887cf4488a17508621c22e15f621e4f94d42
Jack Ha <jackha@gmail.com>
2013-11-15 19:46:41 +0100

    a

--- Adafruit_CharLCD.py
+++ Adafruit_CharLCD.py
@@ -59,17 +59,17 @@ class Adafruit_CharLCD:
         # an explicit GPIO interface to use
         if not GPIO:
             import RPi.GPIO as GPIO
-        	self.GPIO = GPIO
-            self.pin_rs = pin_rs
-            self.pin_e = pin_e
-            self.pins_db = pins_db
+        self.GPIO = GPIO
+        self.pin_rs = pin_rs
+        self.pin_e = pin_e
+        self.pins_db = pins_db
 
-            self.GPIO.setmode(GPIO.BCM)
-            self.GPIO.setup(self.pin_e, GPIO.OUT)
-            self.GPIO.setup(self.pin_rs, GPIO.OUT)
+        self.GPIO.setmode(GPIO.BCM)
+        self.GPIO.setup(self.pin_e, GPIO.OUT)
+        self.GPIO.setup(self.pin_rs, GPIO.OUT)
 
-            for pin in self.pins_db:
-                self.GPIO.setup(pin, GPIO.OUT)
+        for pin in self.pins_db:
+            self.GPIO.setup(pin, GPIO.OUT)
 
         self.write4bits(0x33) # initialization
         self.write4bits(0x32) # initialization
