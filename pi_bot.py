74a7878a65f565d0e5e859be921a09c3e1198788
Jack Ha <jackha@gmail.com>
2013-12-01 20:07:09 +0100

    a

--- pi_bot.py
+++ pi_bot.py
@@ -82,7 +82,7 @@ class Pibot(object):
         4 is x axis, 
         5 is y axis
         """
-        self.pwm.setPWM(4, 0, int((float(x)+1)/2 * (SERVO_MAX-SERVO_MIN) + SERVO_MIN)
+        self.pwm.setPWM(4, 0, int((float(x)+1)/2 * (SERVO_MAX-SERVO_MIN) + SERVO_MIN))
         sleep(0.1)
         self.pwm.setPWM(5, 0, int((float(y)+1)/2 * (SERVO_MAX-SERVO_MIN) + SERVO_MIN))
         sleep(0.1)
